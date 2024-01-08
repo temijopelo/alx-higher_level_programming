0f120da9f9e91a1532c4b361fcb0aaefdbac310f
temijopelo <temijeje@gmail.com>
2024-01-08 08:28:20 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,10 @@
 
 def element_at(my_list, idx):
 
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    """ Get element of a given index """
+
+    if idx < 0 or idx > len(my_list):
+        return
+
+
+    return (my_list[idx])
