c5c84c0ca0e2ccba8a6aa7bb072fd43ff461e855
temijopelo <temijeje@gmail.com>
2024-01-10 18:03:55 -0800

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """ Square root printed  """
+    res = []
+    for i in matrix:
+        res_row = []
+        for sqt in i:
+            res_row(sqt ** 2)
+
+        res.append(res_row)
+
+    return res
