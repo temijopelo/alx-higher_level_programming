f4c804d7adb73e621dc61af2b3c67ef1f0f5719b
temijopelo <temijeje@gmail.com>
2024-02-04 15:49:25 -0800

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Created by temi
+"""
+
 
 class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
     pass
