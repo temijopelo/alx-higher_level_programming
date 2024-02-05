772cd867f07dd9bfdb5ac3cbd51b39234053899c
temijopelo <temijeje@gmail.com>
2024-02-05 14:08:38 -0800

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created
+
+@author: Temi Jeje
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
