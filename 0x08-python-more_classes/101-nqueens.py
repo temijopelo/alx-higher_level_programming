29279a972ddc9d3107c09f810869b4cf7c38feb6
temijopelo <temijeje@gmail.com>
2024-02-25 02:51:30 -0800

    adv solved

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def is_safe(board, row, col, N):
     # Check if there is a queen in the same column or diagonals
     for i in range(row):
@@ -11,16 +12,18 @@ def is_safe(board, row, col, N):
             return False
     return True
 
+
 def solve_nqueens_util(board, row, N, solutions):
     if row == N:
         solutions.append([[i, board[i]] for i in range(N)])
         return
-    
+
     for col in range(N):
         if is_safe(board, row, col, N):
             board[row] = col
             solve_nqueens_util(board, row + 1, N, solutions)
             board[row] = -1
+
 
 def solve_nqueens(N):
     if not N.isdigit():
@@ -40,10 +43,10 @@ def solve_nqueens(N):
     for solution in solutions:
         print(solution)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     solve_nqueens(sys.argv[1])
-
