219fde9cb04866c7079cf5dde3df62b5833f1e55
temijopelo <temijeje@gmail.com>
2024-03-13 09:53:14 -0700

    tasks

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert a row using INSERT
+-- DML query to Inserts a new row in the table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (90, 'Jope School');
