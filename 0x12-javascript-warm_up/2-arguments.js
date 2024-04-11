40df749e4b9803759348b33e79fef6594c829388
temijopelo <temijeje@gmail.com>
2024-04-10 17:11:31 -0700

    semi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argc = process.argv.length;
 if (argc > 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 } else if (argc === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
