f95f67a30d34df3a2a3f4a0b9a9e965736759be3
temijopelo <temijeje@gmail.com>
2024-04-10 17:16:38 -0700

    j

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const number = process.argv[2];
 if (!parseInt(number)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < number; i++) {
-		console.log('X'.repeat(number));
-	}
+  for (let i = 0; i < number; i++) {
+    console.log('X'.repeat(number));
+  }
 }
