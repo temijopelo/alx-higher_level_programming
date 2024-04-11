3f252b4ec39935f99ed674053853b2e2cc028ce9
temijopelo <temijeje@gmail.com>
2024-04-10 18:05:48 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const string of strings) {
-  console.log(string);
+for (let i in strings) {
+  console.log(strings[i]);
 }
